5fe5625d389f76041ea60e6d3bb54533405ced24
Ashlie Warren <ashliewantstocode@gmail.com>
2019-06-05 10:40:52 -0700

    add playlist handlebars file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    console.log("get html routes : ",req.user);
-    if(req.user){
+  app.get("/", function (req, res) {
+    console.log("get html routes : ", req.user);
+    if (req.user) {
       console.log("hay usuario ", req.user);
       return res.render("index");
     }
@@ -13,9 +13,9 @@ module.exports = function(app) {
     res.render("signup");
   });
 
-  app.get("/login", function(req, res) {
-    console.log("login : ",req.user);
-    if(req.user){
+  app.get("/login", function (req, res) {
+    console.log("login : ", req.user);
+    if (req.user) {
       return res.redirect("/");
     }
 
@@ -23,16 +23,43 @@ module.exports = function(app) {
   })
 
   // Load example page and pass in an example by id
-  app.get("/podcast/:id", function(req, res) {
-    db.dbPodcast.findOne({ where: { id: req.params.id } }).then(function(dbPodcast) {
+  app.get("/podcast/:id", function (req, res) {
+    db.dbPodcast.findOne({ where: { id: req.params.id } }).then(function (dbPodcast) {
       res.render("example", {
         podcast: dbPodcast
       });
     });
   });
 
+
+
+  app.get('/playlist', async function (req, res) {
+
+    res.render("playlist", {
+      podcasts: [
+        {
+          title: "Testing 1",
+          audio: "http://google.com",
+          imageurl: "http://placehold.it/200/200"
+        },
+        {
+          title: "Testing 2",
+          audio: "http://google.com",
+          imageurl: "http://placehold.it/200/200"
+        },
+        {
+          title: "Testing 3",
+          audio: "http://google.com",
+          imageurl: "http://placehold.it/200/200"
+        }
+      ]
+    })
+
+
+  });
+
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
